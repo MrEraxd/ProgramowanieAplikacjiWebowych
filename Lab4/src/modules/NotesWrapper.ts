4a0f9dd275a4ef2f64a084dc12e4402f4c6b7a8f
MrEraxd <mreraxd@gmail.com>
2021-05-20 22:07:09 +0200

    Wrapper for all Notes

---
+++ Lab4/src/modules/NotesWrapper.ts
@@ -0,0 +1,15 @@
+export class NotesWrapper{
+    HTMLObj: HTMLDivElement;
+
+    constructor() {
+        this.HTMLObj = document.querySelector("#notes-wrapper");
+    }
+
+    loadNotes(): void{
+        /* Gets info from localstorage and create notes */
+    }
+
+    saveNotes(): void{
+
+    }
+}
