ccc3847023c42894aabc0a9a7d29952b60474ef2
MrEraxd <mreraxd@gmail.com>
2021-05-25 22:17:21 +0200

    Added functionality to NotesWrapper.ts

--- Lab4/src/modules/NotesWrapper.ts
+++ Lab4/src/modules/NotesWrapper.ts
@@ -1,12 +1,33 @@
+import { Note, NoteType } from "./Note";
+import AppStorage from "./AppStorage";
+
+interface notesData{
+    notes: Array<NoteType>;
+}
+
 export class NotesWrapper{
-    HTMLObj: HTMLDivElement;
+    #HTMLObj: HTMLDivElement;
+    appStorage: AppStorage;
 
     constructor() {
-        this.HTMLObj = document.querySelector("#notes-wrapper");
+        this.#HTMLObj = document.querySelector("#notes-wrapper");
+        this.appStorage = new AppStorage();
+        this.loadNotes();
     }
 
     loadNotes(): void{
         /* Gets info from localstorage and create notes */
+        const notesData: notesData | string = this.appStorage.getData<notesData>("notesData");
+
+        if(typeof notesData === "string" || notesData === null){
+            console.log("No notes to display");
+            return;
+        }
+
+        notesData.notes.forEach((noteInfo: NoteType) => {
+            let note = new Note(noteInfo);
+            this.#HTMLObj.appendChild(note.HTMLObj);
+        })
     }
 
     saveNotes(): void{
